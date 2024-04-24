6959fec7da436ee631a4f42d132e6ef15687bcd7
eric <eberber01@gmail.com>
2024-04-24 13:20:38 -0700

    Update TrashBin

--- math_tutor_project/src/main/java/math_tutor_project/TrashBin.java
+++ math_tutor_project/src/main/java/math_tutor_project/TrashBin.java
@@ -4,35 +4,23 @@ import java.awt.*;
 import javax.swing.*;
 
 public class TrashBin extends JComponent {
-    private int x;
-    private int y;
     private Image image;
     private String IMAGE_PATH = "./math_tutor_project/src/main/resources/trash.png";
-
     public TrashBin(int x, int y) {
-        this.x = x;
-        this.y = y;
         this.image = new ImageIcon(IMAGE_PATH).getImage();
+        setLocation(x, y);
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.drawImage(image, x, y, null);
+        g.drawImage(image, getX(), getY(), null);
     }
 
     @Override
     public boolean contains(int x, int y) {
-        return x >= this.x && x <= this.x + image.getWidth(null) &&
-                y >= this.y && y <= this.y + image.getHeight(null);
-    }
-
-    public int getX() {
-        return x;
-    }
-
-    public int getY() {
-        return y;
+        return x >= getX() && x <= getX() + image.getWidth(null) &&
+                y >= getY() && y <= getY() + image.getHeight(null);
     }
 
     public Image getImage(){
