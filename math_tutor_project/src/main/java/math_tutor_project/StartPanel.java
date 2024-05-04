5e585a849aa6d0b8045e77d78dec42e094e62c06
fisherlyon <fwlyon@calpoly.edu>
2024-05-03 23:10:27 -0700

    added test logo

--- math_tutor_project/src/main/java/math_tutor_project/StartPanel.java
+++ math_tutor_project/src/main/java/math_tutor_project/StartPanel.java
@@ -9,4 +9,12 @@ public class StartPanel extends JPanel {
         setBackground(Color.BLACK);
         setLayout(null);
     }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Image logo = new ImageIcon(getClass().getResource("/math-madness2.png")).getImage();
+        g.drawImage(logo, 400, 0, 400, 400, this);
+    }
+
 }
