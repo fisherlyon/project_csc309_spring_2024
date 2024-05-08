38f2fb647b0201a78dfc51a6dc284b271f6abc1e
fisherlyon <fwlyon@calpoly.edu>
2024-05-08 11:52:28 -0700

    small change to metrics.sh

--- metrics.sh
+++ metrics.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 
 dir="./src/main/java/project_csc309_spring_2024"
 
 for file in "$dir"/*.java; do
     filename=$(basename -- "$file")
-    echo -e "\nClass Name: $filename"
+    echo "\nClass Name: $filename"
     java Metrics.java "$file"
 done
